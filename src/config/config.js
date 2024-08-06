a73cbcdfd1c181212d7b99aa1450aa9fb505f47a
José Ignacio Gutiérrez Guzmán <jgutierrez@oseznoapps.com>
2024-08-06 16:42:39 -0500

    Config file

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+// src/config.js
+export const config = {
+    ws: 'ws://localhost:3000',
+    stunServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+
+    ],
+    userNames: {
+        user_one: 'User One',
+        user_two: 'User Two',
+    }
+}
